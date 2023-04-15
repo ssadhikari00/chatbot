f60bb3fa5ffcc29a679fddddea4ad6e79dc59d3d
ssadhikari00 <130820156+ssadhikari00@users.noreply.github.com>
2023-04-15 11:22:48 +0530

    Update movie_chatbot_final.py

--- movie_chatbot_final.py
+++ movie_chatbot_final.py
@@ -9,10 +9,10 @@ Original file is located at
 ### #checking installed libraries/package inside colab and it's verison
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+#from __future__ import absolute_import
+#from __future__ import division
+#from __future__ import print_function
+#from __future__ import unicode_literals
 
 import torch
 from torch.jit import script, trace
